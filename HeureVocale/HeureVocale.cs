9197415f8028a53762a4543cb0bc654edcc3ff62
Dxctor-_Sl3ep <m.demeuleneire@ecoles-epsi.net>
2026-04-28 15:39:30 +0200

    etape 2 fonctionnel et 3

--- HeureVocale/HeureVocale.cs
+++ HeureVocale/HeureVocale.cs
@@ -4,33 +4,36 @@ public class HeureVocale
 {
     public string Convertir(DateTime time)
     {
-
-        string[] heuresEnLettres = 
+        string[] heuresEnLettres =
         [
-            "minuit", "une", "deux", "trois", "quatre", "cinq", 
-            "six", "sept", "huit", "neuf", "dix", "onze", 
-            "midi", "une", "deux", "trois", "quatre", "cinq", 
+            "minuit", "une", "deux", "trois", "quatre", "cinq",
+            "six", "sept", "huit", "neuf", "dix", "onze",
+            "midi", "une", "deux", "trois", "quatre", "cinq",
             "six", "sept", "huit", "neuf", "dix", "onze"
         ];
 
-        for (int i = heuresEnLettres.Length - 1; i >= 0; i--)
-        {
-            if (time.Hour < 12 && time.Hour == i)
-            {
-                return $"{heuresEnLettres[i]} heures pile";
-            }
-            if (time.Hour >= 13 && time.Hour < 16)
-            {
-                return $"{heuresEnLettres[i]} ";
-            }
-            if (time.Hour >= 16 && time.Hour < 23)
-            {
-                return $"{heuresEnLettres[i]} heures du soir";
-            }
-        }
-
         int heure = time.Hour;
         string heureTexte = heuresEnLettres[heure];
-        return $"{heureTexte} heures pile";   
-        }
+
+            if (heure == 0)
+            {
+                return "minuit";
+            }
+            if (heure == 12)
+            {
+                return "midi";
+            }
+
+            if (heure > 0 && heure < 12)
+            {
+                return $"{heureTexte} heures pile";
+            }
+
+            if (heure >= 13 && heure < 18)
+            {
+                return $"{heureTexte} heures de l'après-midi";
+            }
+
+        return $"{heureTexte} heures du soir";
+    }
 }
