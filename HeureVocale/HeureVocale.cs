efa7a05e02555edd589291c730b32970ca2d5e57
Dxctor-_Sl3ep <m.demeuleneire@ecoles-epsi.net>
2026-04-28 15:09:23 +0200

    etape 1 fail

---
+++ HeureVocale/HeureVocale.cs
@@ -0,0 +1,9 @@
+namespace Epsi.Atelier3;
+
+public class HeureVocale
+{
+    public string Convertir(DateTime time)
+    {
+        return "";   
+    }    
+}
---
+++ TestHeureVocale/TestHeureVocale.cs
@@ -0,0 +1,17 @@
+﻿namespace TestHeureVocale;
+
+using Epsi.Atelier3;
+
+[TestClass]
+public sealed class TestHeureVocale
+{
+    [TestMethod]
+    public void Test7hPile()
+    {
+        HeureVocale heureVocale = new ();
+
+        var actual = heureVocale.Convertir(DateTime.Parse("7:00"));
+
+        Assert.AreEqual("sept heures pile", actual);
+    }
+}
